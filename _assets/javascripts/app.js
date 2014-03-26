801dc206c9d9db2515de9f009c0dfbbe81cbc8ef
Santiago Ferreira <santiago.ferreira@wyeworks.com>
2014-03-25 21:07:35 -0300

    Add OpenSpace splash message with fotograma

---
+++ _assets/javascripts/app.js
@@ -0,0 +1,9 @@
+!function($) {
+  $('.splash .images').fotorama({
+      width: '100%',
+      height: '100%',
+      margin: 0,
+      fit: 'cover',
+      nav: false
+    }).data('fotorama');
+}(jQuery);
